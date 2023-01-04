5936a3d981a9a71543c1a4c9d21a32ad99906274
Qi Guo <gq20000926@gmail.com>
2023-01-04 16:08:52 -0500

    update

--- server/schema/SCIDB_test1/dataset.js
+++ server/schema/SCIDB_test1/dataset.js
@@ -31,7 +31,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(128),
       allowNull: true
     },
-    stage: {
+    stage_dpi: {
       type: DataTypes.STRING(128),
       allowNull: true
     },
